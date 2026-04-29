1f9c2e1e4e75c8e4531453ef447f3e139b7cce1e
Pablo Halpern <phalpern@halpernwightsoftware.com>
2026-04-28 21:22:59 -0400

    Implement and test `gate` protocol. Implement `gate_with_IO`

--- include/sim_core.h
+++ include/sim_core.h
@@ -8,6 +8,7 @@
 #define INCLUDED_SIM_CORE
 
 #include <atomic>
+#include <array>
 #include <cassert>
 
 namespace sim {
@@ -127,19 +128,65 @@ public:
   bool get() const;
 };
 
-#if 0
-class GateBase
+/// Abstract Base class for logic gates. Each gate has some number of input
+/// leads, some number of output leads (each having an integral index), and an
+/// `execute` method for performing one tick worth of the logic of the
+/// gate. There is also a method for connecting the input leads to the output
+/// leads of another gate, i.e., to create a (possibly cyclic) directed graph
+/// of gates representing the entire logic circuit.
+class gate
+{
+public:
+  constexpr gate() = default;
+
+  /// To avoid slicing, this (and most polymorphic classes) should not be
+  /// copyable or movable.
+  gate(const gate&) = delete;
+  gate& operator=(const gate&) = delete;
+
+  /// (Virtual) destructor does nothing and it is expected that derived-class
+  /// desstructors will do nothing, too.
+  virtual ~gate();
+
+  /// Return a pointer to the output lead specified by `idx`.
+  /// Precondition: `idx` is in range for this gate.
+  virtual const output_lead* get_output_lead(unsigned idx) const = 0;
+
+  /// Connect this gete's `input_lead`, as specified by `input_idx` to the
+  /// `output_lead` specified by `src_gate` and `src_output_idx`.
+  /// Preconditions: `src_gate` is non-null and `input_idx` and
+  /// `src_output_idx` are in range for their respective gates.
+  virtual void connect_input(unsigned    input_idx,
+                             const gate* src_gate,
+                             unsigned    src_output_idx) = 0;
+
+  /// Do the actual work of the gate. The postcondition is that one tick worth
+  /// of work was done, which might or might not result in a change to one
+  /// or more output leads.
+  virtual void execute() = 0;
+};
+
+/// A partial implemention (still abstract) of the `gate` protocol, handling
+/// all of the interconnection logic. A class derived from `gate_with_IO<In,
+/// Out>` has `In` input leads and `Out` output leads. A derived class that is
+/// made concrete simply by defining the `execute` method.
+template <std::size_t NumInputs, std::size_t NumOutputs>
+class gate_with_IO : public gate
 {
 protected:
+  std::array<input_lead,  NumInputs>  m_inputs;
+  std::array<output_lead, NumOutputs> m_outputs;
 
 public:
+  // Rule of 0. Implicitly non-copyable
 
-  // Connect
-  void connectInput(unsigned        input_idx,
-                    const GateBase* sourceGate,
-                    unsigned        output_idx);
+  const output_lead* get_output_lead(unsigned idx) const override;
+
+  void connect_input(unsigned    input_idx,
+                     const gate* src_gate,
+                     unsigned    src_output_idx) override;
 };
-#endif
+
 
 ///////////////////////////////////////////////////////////////////////////////
 //           Inline and template implementations below this line             //
--- test/sim_core.t.cpp
+++ test/sim_core.t.cpp
@@ -76,6 +76,105 @@ TEST(SimCoreTest, InputOutputLead_ST) {
   }
 }
 
+namespace test_A {
+
+/// Test class that takes no input and simply sets a user-supplied Boolean
+/// value to a single output lead. Used to test the `gate` protocol.
+class bit_source : public sim::gate
+{
+  bool             m_value;
+  sim::output_lead m_out;
+
+public:
+  explicit bit_source(bool initval = false)
+    : m_value(initval), m_out(initval) { }
+
+  /// Set value to set to output lead on `execute`
+  void set_value(bool v) { m_value = v; }
+
+  const sim::output_lead* get_output_lead(unsigned idx) const override
+  {
+    assert(0 == idx);
+    return &m_out;
+  }
+
+  void connect_input(unsigned, const gate* , unsigned) override
+  {
+    // No inputs to connect
+    assert(false);
+  }
+
+  void execute() override { m_out.set(m_value); }
+};
+
+/// Test class that has no output leads but simply makes the value from its
+/// input lead available via a `get_value` method. Used to test the `gate`
+/// protocol.
+class bit_sink : public sim::gate
+{
+  bool            m_value = false;
+  sim::input_lead m_in;
+
+public:
+  bit_sink() = default;
+
+  /// Return the value read from the input lead.
+  bool get_value() const { return m_value; }
+
+  const sim::output_lead* get_output_lead(unsigned) const override
+  {
+    assert(false);  // Precondition check
+    return nullptr;
+  }
+
+  void connect_input(unsigned    input_idx,
+                     const gate* src_gate,
+                     unsigned    src_output_idx) override
+  {
+    assert(0 == input_idx);  // Precondition check
+    m_in.connect_to(src_gate->get_output_lead(src_output_idx));
+    m_value = m_in.get();
+  }
+
+  void execute() override { m_value = m_in.get(); }
+};
+
+} // close namespace test_A
+
+/// Test the `gate` protocol
+TEST(SimCoreTest, gateProtocol) {
+  using namespace test_A;
+
+  bit_source source;
+  bit_sink   sink;
+  sink.connect_input(0, &source, 0);
+
+  EXPECT_FALSE(sink.get_value());
+  source.execute();
+  sink.execute();
+  EXPECT_FALSE(sink.get_value());
+
+  sim::time::advance_clock();
+  EXPECT_FALSE(sink.get_value());
+
+  source.set_value(true);
+  source.execute();
+  sink.execute();
+  EXPECT_FALSE(sink.get_value());  // Change not visible until next cycle
+
+  sim::time::advance_clock();
+  EXPECT_FALSE(sink.get_value());  // Didn't execute yet
+  source.set_value(false);
+  source.execute();
+  sink.execute();
+  EXPECT_TRUE(sink.get_value());  // Value computed in last clock cycle
+
+  sim::time::advance_clock();
+  // source.execute();  // Not needed for this test
+  sink.execute();
+  EXPECT_FALSE(sink.get_value());  // Value propagated thrugh
+}
+
 // Local Variables:
 // c-basic-offset: 2
 // End:
